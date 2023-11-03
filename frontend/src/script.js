bda6e293acdd0c0ac78b11e5934b7f20b2d0f395
andreistr21 <andstra45@gmail.com>
2023-11-03 12:57:47 +0100

    Added login page

---
+++ frontend/src/script.js
@@ -0,0 +1,45 @@
+function login(username, password) {
+  fetch("http://127.0.0.1:8000/user/login/", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const responseData = JSON.parse(data);
+      if (responseData.detail === "Login successful.") {
+        console.log("Login successful");
+        document.cookie =
+          "Authorization=Token " + responseData.token + "; path=/";
+      } else {
+        console.log("Login failed");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function loginFormEventListener(loginForm) {
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    login(username, password);
+  });
+}
+
+window.addEventListener("load", function () {
+  const loginForm = document.getElementById("login-form");
+
+  if (loginForm) {
+    loginFormEventListener(loginForm);
+  }
+});
