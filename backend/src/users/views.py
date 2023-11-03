c8eecb22e66095abd41cf004bef1308549b8979e
andreistr21 <andstra45@gmail.com>
2023-11-03 12:57:17 +0100

    Added check if user already logged in

--- backend/src/users/views.py
+++ backend/src/users/views.py
@@ -14,24 +14,25 @@ class UserLoginView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        if request.user.is_authenticated:
-            return Response(
-                {"detail": "You are already logged in."},
-                status=status.HTTP_200_OK,
-            )
+        if authorization_header := request.META.get("HTTP_COOKIE"):
+            token = authorization_header.split(" ")[1]
+            if Token.objects.filter(key=token).first():
+                return Response(
+                    {"detail": "You are already logged in."},
+                    status=status.HTTP_200_OK,
+                )
 
         serializer = UserLoginSerializer(data=request.data)
-
         if serializer.is_valid():
             user = serializer.validated_data["user"]
             if user is not None:
                 login(request, user)
-                token, _ = Token.objects.get_or_create(user=user)  # type: ignore
+                token, _ = Token.objects.get_or_create(user=user)
                 user_serializer = UserSerializer(user)
                 data = {
                     "detail": "Login successful.",
                     "user": user_serializer.data,
-                    "token": token,
+                    "token": token.key,
                 }
                 return Response(
                     json.dumps(data),
